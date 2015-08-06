ce8a12160a96551a362af769f7d0846689c50d2c
BlueNebula <kiwlgus1@korea.ac.kr>
2015-08-06 11:35:00 +0900

    (i) add member_add page

---
+++ src/application/views/fragment/member_add.php
@@ -0,0 +1,32 @@
+<div class="dv_member_add">
+    <style scoped>
+
+    </style>
+
+    <div class="dv_email">
+        User Email <br>
+        <input type="text" class="email" name="email">
+    </div>
+
+    <div class="dv_password">
+        Password <br>
+        <input type="password" class="password" name="password">
+        Password Confirm <br>
+        <input type="password" class="password_cfm" name="password_cfm">
+    </div>
+
+    <div class="dv_nickname">
+        Nickname <br>
+        <input type="text" class="nickname" name="nickname">
+    </div>
+
+    <div class="dv_photo">
+        Photo <br>
+        <input type="file" class="photo" name="photo">
+        <div class="dv_preview"></div>
+    </div>
+
+    <script>
+
+    </script>
+</div>
