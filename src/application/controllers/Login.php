38f47be9ccba53a46ede716021000a53bd466a21
BlueNebula <kiwlgus1@korea.ac.kr>
2015-08-07 18:04:19 +0900

    (e) edit main function, (get method requestd)

--- src/application/controllers/Login.php
+++ src/application/controllers/Login.php
@@ -2,18 +2,25 @@
 /**
  * Login file.
  */
-
 	// Debugging.
 	__debug_load(__FILE__);
-
 
 /**
  * 
  */
 class Login extends Controller {
 
-	function main($url = null) {
+	function __construct($view = null){
+		parent::__construct();
+	}
 
+	function main($url = null) {
+		if(strtolower($_SERVER["REQUEST_METHOD"]) == "get"){
+			//todo : if request method is get,
+			require_once APPPATH.'views'.DS.'templates'.DS.'template_login.php';
+		}else{
+			//todo : if request method is post
+		}
 	}
 
 	function render($file_template){
@@ -21,7 +28,4 @@ class Login extends Controller {
 	}
 
 }
-
-
-
 ?>
