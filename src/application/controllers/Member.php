d0ed41aa9a5cba086981f5b593006c91afa2154a
BlueNebula <kiwlgus1@korea.ac.kr>
2015-08-07 13:49:23 +0900

    (i) implement main(create) method in Member Controller

--- src/application/controllers/Member.php
+++ src/application/controllers/Member.php
@@ -42,9 +42,34 @@ class Member extends Controller
                     require_once APPPATH.'views'.DS.'templates'.DS.'template_member_add.php';
                 }else{
                     //todo : create new member
-                    $member = array();
-                    var_dump($_POST);
-                    var_dump($_FILES);
+                    if(isset($_FILES["photo"]["tmp_name"])
+                        && isset($_POST["email"]) && isset($_POST["password"])
+                        && isset($_POST["password_cfm"]) && isset($_POST["nickname"])){
+                        if(!strcmp($_POST["password"],$_POST["password_cfm"])){
+                            $image_info = getimagesize($_FILES['photo']['tmp_name']);
+                            //todo : divide maximum file size by using constant in config.xml
+                            if($_FILES["photo"]["size"] < 10000000)
+
+                            $member = array();
+                            $member["email"] = $_POST["email"];
+                            $member["password"] = $_POST["password"];
+                            $member["nickname"] = $_POST["nickname"];
+                            //todo : image resize (make thumbnail profile image)
+                            $member["img"] = fopen($_FILES["photo"]["tmp_name"],"rb");
+
+                            var_dump($member);
+
+                            if(Core::getInstance("Member_md")->addMember($member)){
+                                //todo : return success page
+                            }else{
+                                //todo : redirect 500 page
+                            }
+                        }else{
+                            //todo : invalid password
+                        }
+                    }else{
+                        //todo : invalid parameter
+                    }
                 }
                 break;
             case "update":
--- src/application/models/Member_md.php
+++ src/application/models/Member_md.php
@@ -2,24 +2,20 @@
 /**
  * Term_md file.
  */
-
-
 	// Debugging.
 	__debug_load(__FILE__);
-
-
-
-
-
 /**
  * 
  */
 class Member_md extends Model {
 	
-	function main(){
+	function addMember($member){
+		try{
 
+		}catch(Exception $e){
+			throw new Exception("Can't insert member,".json_encode($member)." ".$e);
+		}
 	}
-
 }
 
 
