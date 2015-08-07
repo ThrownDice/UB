2f6f2747dac8f3fda899574104a8204a0800f4c5
BlueNebula <kiwlgus1@korea.ac.kr>
2015-08-07 18:36:51 +0900

    (e) update login.php, css, add signup login btn

--- src/application/views/fragment/login.php
+++ src/application/views/fragment/login.php
@@ -2,57 +2,88 @@
     <style scoped>
 
         .dv_login{
-            width : 250px;
+            width : 400px;
             margin : 0px auto;
             margin-top : 50px;
+            background : white;
+            padding-bottom : 10pt;
+            border : 2px solid #2D79B2;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
         }
 
         .dv_header{
-            background : #FFAD0B;
+            background : #2D79B2;
             color : white;
-            font-weight: bold;
             border-top-left-radius: 5px;
             border-top-right-radius: 5px;
             font-size : 15pt;
         }
 
         .dv_email, .dv_password{
-            font-size : 10pt;
+            font-size : 12pt;
             color : dimgray;
+            margin-top : 10px;
+            text-align : center;
+            height : 50px;
         }
 
         input{
-            width : 100%;
+            width : 80%;
             border-radius: 10px;
             border : 1px solid lightgray;
+            margin : 0px auto;
+            padding : 2px 5px 2px 5px;
         }
 
         input:focus{
             border-color : #FFAD0B;
         }
+
+        .btn{
+            height : 40px;
+            background : #2D79B2;
+            color : white;
+            line-height : 40px;
+            text-align : center;
+            margin : 10px;
+            padding : 0px 10px 0px 10px;
+            -webkit-border-radius : 5px;
+            -moz-border-radius : 5px;
+            border-radius : 5px;
+            font-size : 11pt;
+            cursor : pointer;
+        }
+
+        .btn:hover{
+            background : #FFAD0B;
+        }
     </style>
 
-    <form action="/login" method="post">
-    <div class="dv_header"> <!--로그인--> &nbsp;Login </div>
+    <form action="/login" method="post" class="fm_login">
+    <div class="dv_header"> &nbsp;로그인 <!--&nbsp;Login--> </div>
     <div class="dv_email">
-        <!--이메일--> Email <br>
+        이메일 <!--Email--> <br>
         <input type="text" class="email" name="email">
     </div>
     <div class="dv_password">
-        <!--비밀번호--> Password <br>
+        비밀번호 <!--Password--> <br>
         <input type="password" class="password" name="password">
     </div>
-
+    <div class="dv_menu">
+        <div class="btn login"> 로그인 </div>
+        <div class="btn sign-up"> 회원가입 </div>
+    </div>
     </form>
 
     <script>
-
+        (function(window, document){
+            $('.sign-up').on('click', function(){
+                location.href = "member?action=create";
+            });
+            $('.login').on('click', function(){
+                $('.fm_login').submit();
+            })
+        })(window, document);
     </script>
 </div>
-<?php
-/**
- * Created by PhpStorm.
- * User: TD
- * Date: 2015-08-07
- * Time: 오후 5:06
- */
