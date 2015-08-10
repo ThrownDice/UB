ec55accfec89484d6cf052a3a6781f1a5dabd0cd
BlueNebula <kiwlgus1@korea.ac.kr>
2015-08-10 17:14:27 +0900

    implement likeTerm, dislikeTerm method, add prototype getTermLogByMemberId, getTermLogByTermId

--- src/application/models/Vote_md.php
+++ src/application/models/Vote_md.php
@@ -12,11 +12,45 @@ class Vote_md extends Model
     function __construct(){}
 
     function likeTerm($term_id, $member_id){
+        try{
+            if(!empty($term_id) && !empty($member_id)){
+                $db = self::getDatabase();
+                $stmt = $db->prepare("insert into vote (flag, term_id, member_id) values(:flag, :term_id, :member_id)");
 
+                $stmt->bindParam(":flag", 1, PDO::PARAM_INT);
+                $stmt->bindParam(":term_id", $term_id, PDO::PARAM_INT);
+                $stmt->bindParam(":member_id", $member_id, PDO::PARAM_INT);
+
+                $stmt->execute();
+                return true;
+            }else{
+                //todo : invalid parameter
+                return false;
+            }
+        }catch(Exception $e){
+            throw new Exception("Can't insert term log. ".$e);
+        }
     }
 
     function dislikeTerm($term_id, $member_id){
+        try{
+            if(!empty($term_id) && !empty($member_id)){
+                $db = self::getDatabase();
+                $stmt = $db->prepare("insert into vote (flag, term_id, member_id) values(:flag, :term_id, :member_id)");
 
+                $stmt->bindParam(":flag", -1, PDO::PARAM_INT);
+                $stmt->bindParam(":term_id", $term_id, PDO::PARAM_INT);
+                $stmt->bindParam(":member_id", $member_id, PDO::PARAM_INT);
+
+                $stmt->execute();
+                return true;
+            }else{
+                //todo : invalid parameter
+                return false;
+            }
+        }catch(Exception $e){
+            throw new Exception("Can't insert term log. ".$e);
+        }
     }
 
     function likeComment($comment_id){
@@ -39,8 +73,12 @@ class Vote_md extends Model
 
     }
 
-    function getTermLog($member_id){
+    function getTermLogByMemberId($member_id){
 
+    }
+
+    function getTermLogByTermId($term_id){
+        
     }
 
 }
