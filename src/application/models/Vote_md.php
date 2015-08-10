2f02079cb3c2a7598f32fa96339d69bd716b0e01
BlueNebula <kiwlgus1@korea.ac.kr>
2015-08-10 16:50:27 +0900

    (a) add Vote_md Class

---
+++ src/application/models/Vote_md.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: TD
+ * Date: 2015-08-10
+ * Time: 오후 2:21
+ */
+class Vote_md extends Model
+{
+
+    function __construct(){}
+
+    function likeTerm($term_id, $member_id){
+
+    }
+
+    function dislikeTerm($term_id, $member_id){
+
+    }
+
+    function likeComment($comment_id){
+
+    }
+
+    function dislikeComment($comment_id){
+
+    }
+
+    function calcPopTerm($term_id){
+
+    }
+
+    function calcPopComment($comment_id){
+
+    }
+
+    function deleteTermLog($term_id, $member_id){
+
+    }
+
+    function getTermLog($member_id){
+
+    }
+
+}
